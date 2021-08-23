6fce03dc4276f8153cc3fb0c4b7c398c000bedfc
SuperDroidBond <superdroidbond@yahoo.com>
2021-08-23 19:56:07 +0530

    ChagingAnimation: add option for BG Dim to Alpha or Black [1/2]
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6949,6 +6949,12 @@ public final class Settings {
         public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
         /**
+         * Select which System charging animation bg to display
+         * @hide
+         */
+        public static final String CHARGING_ANIMATION_BG = "charging_animation_bg";
+
+	/**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
