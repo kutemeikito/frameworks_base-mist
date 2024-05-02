CherishOS/android_frameworks_base

SystemUI: Add USB Tether tile
Author: BestPig <bestpig82@gmail.com>
Date:   Sun Feb 1 03:04:41 2015 +0100

    SystemUI: add USB Tether tile

     * Added vector drawables (blunden)

     * Long click action changed to open Tethering options, in the same way
       as Google does for Hotspot tile (bgcngm)

     * USB Tethering enablement check refactored (bgcngm and luca020400)

    Change-Id: Ic44f2b596fe32152f3f722347a855325c52113c6

Author: Gabriele M <moto.falcon.git@gmail.com>
Date:   Fri Apr 27 20:51:42 2018 +0200

    SystemUI: Toggle USB tethering only when USB is connected

    Pressing the tile when unplugged causes USB tethering to turn on
    as soon as the cable is connected. This can happen long after the
    tile has been pressed, so don't do anything if unplugged.

    Change-Id: Ib8bbdc758097ca7af5d4e7c3483c28ba42fb9ce1

Change-Id: Ibbc0ba9de2cc3eee2a7f6a9c9f005730244f0b3a
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/UsbTetherTile.java
@@ -1,0 +1,172 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2017-2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbManager;
+import android.net.TetheringManager;
+import android.net.TetheringManager.TetheringRequest;
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.Settings;
+import android.net.ConnectivityManager;
+import android.service.quicksettings.Tile;
+import android.view.View;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.android.internal.util.ConcurrentUtils;
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.R;
+import com.android.systemui.dagger.qualifiers.Background;
+import com.android.systemui.dagger.qualifiers.Main;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.plugins.FalsingManager;
+import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.qs.logging.QSLogger;
+
+import javax.inject.Inject;
+
+import dagger.Lazy;
+
+/**
+ * USB Tether quick settings tile
+ */
+public class UsbTetherTile extends QSTileImpl<BooleanState> {
+
+    public static final String TILE_SPEC = "usb_tether";
+
+    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_usb_tether);
+
+    private final String TAG = "UsbTetherTile";
+
+    private static final Intent TETHER_SETTINGS = new Intent().setComponent(new ComponentName(
+            "com.android.settings", "com.android.settings.TetherSettings"));
+
+    private final TetheringManager mTetheringManager;
+
+    private boolean mListening;
+    private Handler mMainHandler;;
+    private boolean mUsbConnected = false;
+    private boolean mUsbTetherEnabled = false;
+
+    @Inject
+    public UsbTetherTile(
+            QSHost host,
+            @Background Looper backgroundLooper,
+            @Main Handler mainHandler,
+            FalsingManager falsingManager,
+            MetricsLogger metricsLogger,
+            StatusBarStateController statusBarStateController,
+            ActivityStarter activityStarter,
+            QSLogger qsLogger
+    ) {
+        super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
+                statusBarStateController, activityStarter, qsLogger);
+        mMainHandler = mainHandler;
+        mTetheringManager = mContext.getSystemService(TetheringManager.class);
+    }
+
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        if (mListening == listening) {
+            return;
+        }
+        mListening = listening;
+        if (listening) {
+            final IntentFilter filter = new IntentFilter();
+            filter.addAction(UsbManager.ACTION_USB_STATE);
+            mContext.registerReceiver(mReceiver, filter);
+        } else {
+            mContext.unregisterReceiver(mReceiver);
+        }
+    }
+
+    @Override
+    protected void handleClick(@Nullable View view) {
+        if (!mUsbConnected) {
+            return;
+        }
+
+        if (!mUsbTetherEnabled) {
+            Log.d(TAG, "Starting Usb tethering");
+            mTetheringManager.startTethering(new TetheringRequest.Builder(
+                    TetheringManager.TETHERING_USB).setShouldShowEntitlementUi(false).build(),
+                    ConcurrentUtils.DIRECT_EXECUTOR,
+                    new TetheringManager.StartTetheringCallback() {
+                        @Override
+                        public void onTetheringFailed(final int result) {
+                            Log.e(TAG, "onTetheringFailed");
+                        }
+                    });
+        } else {
+            mTetheringManager.stopTethering(TetheringManager.TETHERING_USB);
+        }
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return new Intent(TETHER_SETTINGS);
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            mUsbConnected = intent.getBooleanExtra(UsbManager.USB_CONNECTED, false);
+            if (mUsbConnected && mTetheringManager.isTetheringSupported()) {
+                mUsbTetherEnabled = intent.getBooleanExtra(UsbManager.USB_FUNCTION_RNDIS, false);
+            } else {
+                mUsbTetherEnabled = false;
+            }
+            refreshState();
+        }
+    };
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.value = mUsbTetherEnabled;
+        state.label = mContext.getString(R.string.quick_settings_usb_tether_label);
+        state.icon = mIcon;
+        state.state = !mUsbConnected ? Tile.STATE_UNAVAILABLE
+                : mUsbTetherEnabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_usb_tether_label);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CHERISH_SETTINGS;
+    }
+}

kutemeikito/frameworks_base-mist

packages/SystemUI/src/com/android/systemui/qs/tiles/UsbTetherTile.java
@@ -1,0 +1,172 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2017-2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbManager;
+import android.net.TetheringManager;
+import android.net.TetheringManager.TetheringRequest;
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.Settings;
+import android.net.ConnectivityManager;
+import android.service.quicksettings.Tile;
+import android.view.View;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.android.internal.util.ConcurrentUtils;
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.R;
+import com.android.systemui.dagger.qualifiers.Background;
+import com.android.systemui.dagger.qualifiers.Main;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.plugins.FalsingManager;
+import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.qs.logging.QSLogger;
+
+import javax.inject.Inject;
+
+import dagger.Lazy;
+
+/**
+ * USB Tether quick settings tile
+ */
+public class UsbTetherTile extends QSTileImpl<BooleanState> {
+
+    public static final String TILE_SPEC = "usb_tether";
+
+    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_usb_tether);
+
+    private final String TAG = "UsbTetherTile";
+
+    private static final Intent TETHER_SETTINGS = new Intent().setComponent(new ComponentName(
+            "com.android.settings", "com.android.settings.TetherSettings"));
+
+    private final TetheringManager mTetheringManager;
+
+    private boolean mListening;
+    private Handler mMainHandler;;
+    private boolean mUsbConnected = false;
+    private boolean mUsbTetherEnabled = false;
+
+    @Inject
+    public UsbTetherTile(
+            QSHost host,
+            @Background Looper backgroundLooper,
+            @Main Handler mainHandler,
+            FalsingManager falsingManager,
+            MetricsLogger metricsLogger,
+            StatusBarStateController statusBarStateController,
+            ActivityStarter activityStarter,
+            QSLogger qsLogger
+    ) {
+        super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
+                statusBarStateController, activityStarter, qsLogger);
+        mMainHandler = mainHandler;
+        mTetheringManager = mContext.getSystemService(TetheringManager.class);
+    }
+
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        if (mListening == listening) {
+            return;
+        }
+        mListening = listening;
+        if (listening) {
+            final IntentFilter filter = new IntentFilter();
+            filter.addAction(UsbManager.ACTION_USB_STATE);
+            mContext.registerReceiver(mReceiver, filter);
+        } else {
+            mContext.unregisterReceiver(mReceiver);
+        }
+    }
+
+    @Override
+    protected void handleClick(@Nullable View view) {
+        if (!mUsbConnected) {
+            return;
+        }
+
+        if (!mUsbTetherEnabled) {
+            Log.d(TAG, "Starting Usb tethering");
+            mTetheringManager.startTethering(new TetheringRequest.Builder(
+                    TetheringManager.TETHERING_USB).setShouldShowEntitlementUi(false).build(),
+                    ConcurrentUtils.DIRECT_EXECUTOR,
+                    new TetheringManager.StartTetheringCallback() {
+                        @Override
+                        public void onTetheringFailed(final int result) {
+                            Log.e(TAG, "onTetheringFailed");
+                        }
+                    });
+        } else {
+            mTetheringManager.stopTethering(TetheringManager.TETHERING_USB);
+        }
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return new Intent(TETHER_SETTINGS);
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            mUsbConnected = intent.getBooleanExtra(UsbManager.USB_CONNECTED, false);
+            if (mUsbConnected && mTetheringManager.isTetheringSupported()) {
+                mUsbTetherEnabled = intent.getBooleanExtra(UsbManager.USB_FUNCTION_RNDIS, false);
+            } else {
+                mUsbTetherEnabled = false;
+            }
+            refreshState();
+        }
+    };
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.value = mUsbTetherEnabled;
+        state.label = mContext.getString(R.string.quick_settings_usb_tether_label);
+        state.icon = mIcon;
+        state.state = !mUsbConnected ? Tile.STATE_UNAVAILABLE
+                : mUsbTetherEnabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_usb_tether_label);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.MIST_SETTINGS;
+    }
+}