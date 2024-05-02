CherishOS/android_frameworks_base

[SQUASH] PixelPropsUtils: Various changes
* Allow enabling debug via `persist.sys.pixelprops.debug` prop.
* Bring back Pixel checks.
* Bring back Snapchat spoof toggle.
* Spoof GMS processes that check for tensor to Pixel 5a.
* Spoof most Google/Samsung apps/processes back to Pixel 5a by default.
* Spoof select Google apps/processes to Pixel 8 Pro to enable exclusive features.
* Turn off spoof for select Google apps once again.

PixelPropsUtils: Fix bootloader spoofing

* aside from sign-in failing due to empty hardware identifier,
  earlier patch also introduced another regression which resulted to breakage of sign-up,
  fix it by using propimitation hooks `onEngineGetCertificateChain` and blacklisting setupwizard.

test: do factory reset, do device setup, google sign-in works and setup completed without errors

Change-Id: Ia81ed7c9c07d42ff975f059fda8e45b10e2a457d
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

PixelPropUtils: unspoof some packages
Signed-off-by: aswin7469 <aswinas@pixysos.com>

Revert "PixelPropsUtils: Refactor and cleanup spoofing"

This reverts commit fbd5aa372a977207edb5f45ccdce4a85ea1bca9e.

Revert "ApplicationPackageManager: Add a config for device with Tensor SoC"

This reverts commit 50459e3a635f7932c7caac9fd1ba2e621b1a70f4.

Revert "PPU: Stop spoofing com.google.android.googlequicksearchbox"

This reverts commit e4528e6188d28a2270246e4f3e250eb1f3e33d3f.

Revert "PixelPropsUtils: Drop pixel codenames and checks"

This reverts commit 195366ea6c84871fc4228ace62620f22ed2c11d5.

Revert "core: Allow force enabling tensor feature XMLs on non-Pixels [1/2]"

This reverts commit ea1983f86787702f930f0653820836eb3f6c1e97.

Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
Co-authored-by: aswin7469 <aswinas@pixysos.com>
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

core/java/android/app/ApplicationPackageManager.java
@@ -893,16 +893,9 @@ public class ApplicationPackageManager extends PackageManager {
 
     @Override
     public boolean hasSystemFeature(String name, int version) {
-        if (name != null && Arrays.asList(featuresTensor).contains(name)
-                && !Arrays.asList(pTensorCodenames).contains(SystemProperties.get("ro.cherish.device"))) {
-            return false;
-        }
         String packageName = ActivityThread.currentPackageName();
-        if ((!Arrays.asList(pTensorCodenames).contains(SystemProperties.get("ro.cherish.device"))
-                && SystemProperties.getBoolean("persist.sys.tensor.features", false))
-                && (packageName != null
-                && packageName.toLowerCase().contains("com.google")
-                && !packageName.equals("com.google.android.apps.photos"))) {
+        if (name != null
+                && Arrays.asList(pTensorCodenames).contains(SystemProperties.get("ro.cherish.device"))) {
             if (Arrays.asList(featuresTensor).contains(name)) return true;
         }
         if (packageName != null

kutemeikito/frameworks_base-mist

core/java/android/app/ApplicationPackageManager.java
@@ -893,16 +893,9 @@ public class ApplicationPackageManager extends PackageManager {
 
     @Override
     public boolean hasSystemFeature(String name, int version) {
-        if (name != null && Arrays.asList(featuresTensor).contains(name)
-                && !Arrays.asList(pTensorCodenames).contains(SystemProperties.get("ro.mist.device"))) {
-            return false;
-        }
         String packageName = ActivityThread.currentPackageName();
-        if ((!Arrays.asList(pTensorCodenames).contains(SystemProperties.get("ro.mist.device"))
-                && SystemProperties.getBoolean("persist.sys.tensor.features", false))
-                && (packageName != null
-                && packageName.toLowerCase().contains("com.google")
-                && !packageName.equals("com.google.android.apps.photos"))) {
+        if (name != null
+                && Arrays.asList(pTensorCodenames).contains(SystemProperties.get("ro.mist.device"))) {
             if (Arrays.asList(featuresTensor).contains(name)) return true;
         }
         if (packageName != null