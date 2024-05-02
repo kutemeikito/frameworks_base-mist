CherishOS/android_frameworks_base

ApplicationPackageManager: Rearrange Tensor feature checks
* Move googlequicksearchbox to upper to correctly enable circle to search

Change-Id: I6646275998c52494db712faa07587690d323afbd
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

core/java/android/app/ApplicationPackageManager.java
@@ -894,22 +894,22 @@ public class ApplicationPackageManager extends PackageManager {
     @Override
     public boolean hasSystemFeature(String name, int version) {
         String packageName = ActivityThread.currentPackageName();
-        if (name != null
-                && Arrays.asList(pTensorCodenames).contains(SystemProperties.get("ro.cherish.device"))) {
+        if (packageName != null
+                && packageName.equals("com.google.android.googlequicksearchbox")) {
+            if (Arrays.asList(featuresPixel).contains(name)) return true;
+            if (Arrays.asList(featuresPixelOthers).contains(name)) return true;
             if (Arrays.asList(featuresTensor).contains(name)) return true;
+            if (Arrays.asList(featuresNexus).contains(name)) return true;
+        }
+        if (name != null && Arrays.asList(featuresTensor).contains(name)
+                && !Arrays.asList(pTensorCodenames).contains(SystemProperties.get("ro.cherish.device"))) {
+            return false;
         }
         if (packageName != null
                 && packageName.equals("com.google.android.apps.photos")
                 && SystemProperties.getBoolean("persist.sys.pixelprops.gphotos", true)) {
             if (Arrays.asList(featuresPixel).contains(name)) return false;
             if (Arrays.asList(featuresPixelOthers).contains(name)) return true;
-            if (Arrays.asList(featuresNexus).contains(name)) return true;
-        }
-        if (packageName != null
-                && packageName.equals("com.google.android.googlequicksearchbox")) {
-            if (Arrays.asList(featuresPixel).contains(name)) return true;
-            if (Arrays.asList(featuresPixelOthers).contains(name)) return true;
-            if (Arrays.asList(featuresTensor).contains(name)) return true;
             if (Arrays.asList(featuresNexus).contains(name)) return true;
         }
         if (Arrays.asList(featuresAndroid).contains(name)) return true;

kutemeikito/frameworks_base-mist

core/java/android/app/ApplicationPackageManager.java
@@ -894,22 +894,22 @@ public class ApplicationPackageManager extends PackageManager {
     @Override
     public boolean hasSystemFeature(String name, int version) {
         String packageName = ActivityThread.currentPackageName();
-        if (name != null
-                && Arrays.asList(pTensorCodenames).contains(SystemProperties.get("ro.mist.device"))) {
+        if (packageName != null
+                && packageName.equals("com.google.android.googlequicksearchbox")) {
+            if (Arrays.asList(featuresPixel).contains(name)) return true;
+            if (Arrays.asList(featuresPixelOthers).contains(name)) return true;
             if (Arrays.asList(featuresTensor).contains(name)) return true;
+            if (Arrays.asList(featuresNexus).contains(name)) return true;
+        }
+        if (name != null && Arrays.asList(featuresTensor).contains(name)
+                && !Arrays.asList(pTensorCodenames).contains(SystemProperties.get("ro.mist.device"))) {
+            return false;
         }
         if (packageName != null
                 && packageName.equals("com.google.android.apps.photos")
                 && SystemProperties.getBoolean("persist.sys.pixelprops.gphotos", true)) {
             if (Arrays.asList(featuresPixel).contains(name)) return false;
             if (Arrays.asList(featuresPixelOthers).contains(name)) return true;
-            if (Arrays.asList(featuresNexus).contains(name)) return true;
-        }
-        if (packageName != null
-                && packageName.equals("com.google.android.googlequicksearchbox")) {
-            if (Arrays.asList(featuresPixel).contains(name)) return true;
-            if (Arrays.asList(featuresPixelOthers).contains(name)) return true;
-            if (Arrays.asList(featuresTensor).contains(name)) return true;
             if (Arrays.asList(featuresNexus).contains(name)) return true;
         }
         if (Arrays.asList(featuresAndroid).contains(name)) return true;