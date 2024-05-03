Evolution-X-AOSP/frameworks_base

Gesture nav: Add back show recents action
Change-Id: I2a487bd6752724840a018cbe2cb369f7d774f876
Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1388,6 +1388,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             case 15: // Partial Screenshot
                 EvolutionUtils.takeScreenshot(false);
                 break;
+            case 16: // Recents
+                EvolutionUtils.sendKeycode(KeyEvent.KEYCODE_APP_SWITCH);
+                break;
         }
     }
 

kutemeikito/frameworks_base-mist

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1387,6 +1387,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             case 15: // Partial Screenshot
                 MistUtils.takeScreenshot(false);
                 break;
+            case 16: // Recents
+                MistUtils.sendKeycode(KeyEvent.KEYCODE_APP_SWITCH);
+                break;
         }
     }
 