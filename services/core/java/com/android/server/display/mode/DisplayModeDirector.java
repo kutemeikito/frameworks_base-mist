Evolution-X-AOSP/frameworks_base

Allow user to select low power refresh rate [1/2]
* This patch is based on:
  https://github.com/crdroidandroid/android_frameworks_base/commit/9d4b0238e0c2caa810bdbf1c3d5e1c65838ef2e4
* With newer devices supporting refresh rates lower than 60 Hz,
  let user customize this further.

Squashed with the revert of the old one:

Revert "base: Allow disabling refresh rate lowering in battery saver"

This reverts commit a4b15676366a882a2048ded9a57e2ee76ad80186.

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: Joey Huab <joey@evolution-x.org>

services/core/java/com/android/server/display/DisplayModeDirector.java
@@ -1203,13 +1203,14 @@ public class DisplayModeDirector {
         }
 
         private void updateLowPowerModeSettingLocked() {
-            boolean inLowPowerMode = Settings.Global.getInt(mContext.getContentResolver(),
+            final ContentResolver cr = mContext.getContentResolver();
+            boolean inLowPowerMode = Settings.Global.getInt(cr,
                     Settings.Global.LOW_POWER_MODE, 0 /*default*/) != 0;
-            boolean shouldSwitchRefreshRate = Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.LOW_POWER_REFRESH_RATE, 1 /*default*/) != 0;
             final Vote vote;
-            if (inLowPowerMode && shouldSwitchRefreshRate) {
-                vote = Vote.forRefreshRates(0f, 60f);
+            if (inLowPowerMode) {
+                float lowPowerRefreshRate = Settings.System.getFloatForUser(cr,
+                    Settings.System.LOW_POWER_REFRESH_RATE, 60f /*default*/, cr.getUserId());
+                vote = Vote.forRefreshRates(0f, lowPowerRefreshRate);
             } else {
                 vote = null;
             }

kutemeikito/frameworks_base-mist

services/core/java/com/android/server/display/mode/DisplayModeDirector.java
@@ -966,7 +966,8 @@ public class DisplayModeDirector {
         }
 
         private void updateLowPowerModeSettingLocked() {
-            boolean inLowPowerMode = Settings.Global.getInt(mContext.getContentResolver(),
+            final ContentResolver cr = mContext.getContentResolver();
+            boolean inLowPowerMode = Settings.Global.getInt(cr,
                     Settings.Global.LOW_POWER_MODE, 0 /*default*/) != 0;
             final Vote vote;
             if (inLowPowerMode && mVsynLowPowerVoteEnabled) {
@@ -977,7 +978,9 @@ public class DisplayModeDirector {
                                 /* vsyncRate= */ 60f)
                 ));
             } else if (inLowPowerMode) {
-                vote = Vote.forRenderFrameRates(0f, 60f);
+                float lowPowerRefreshRate = Settings.System.getFloatForUser(cr,
+                    Settings.System.LOW_POWER_REFRESH_RATE, 60f /*default*/, cr.getUserId());
+                vote = Vote.forRenderFrameRates(0f, lowPowerRefreshRate);
             } else {
                 vote = null;
             }