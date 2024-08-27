c9589c16029cae767c521029031a0a02ca291362
ShukakuZa <yusufzuzu787@gmail.com>
2024-08-27 20:40:33 +0530

    Revert "[SQUASH] Advanced navigation "extended/L" gestures"
    This reverts commit 6f7848e6b6176d786376f897a1b0c9e35614cf04.

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
@@ -621,7 +621,6 @@ public class NavigationBarView extends FrameLayout implements
             mTransitionListener.onBackAltCleared();
         }
         mImeVisible = visible;
-        mEdgeBackGestureHandler.setImeVisible(visible);
         mRotationButtonController.getRotationButton().setCanShowRotationButton(!visible && mIsUserEnabled);
     }
 
